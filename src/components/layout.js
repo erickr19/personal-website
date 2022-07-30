0aee31d83c0a149f2f21e0de8be1863f360f9f93
Erick Reyes <erickreyes1902@gmail.com>
2022-07-30 18:52:05 -0500

    Added a function to find user agent and rendering an appropriate display to improve iOS UX

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,29 +2,52 @@
 import * as React from 'react';
 import { HomeIcon, InformationCircleIcon } from '@heroicons/react/outline';
 import { Link } from "gatsby";
-
+import detectAgent from '../functions/detect-agent';
 // define layout
 
 const Layout = ({ pageTitle, children }) => {
-    // TODO: Add dynamic page titles
-    
-    return (
-        <div className="px-2 md:p-0 flex flex-col align-middle justify-center h-screen bg-gradient-to-t from-slate-900 to-cyan-900">
-            <title>{pageTitle}</title>
-            {/* TODO: add nav for desktop */}
-            {/* card container */}
-            <div className="self-center md:rounded-lg h-5/6 w-screen sm:w-4/5 overflow-auto">
-                <main>
-                    {children}
-                </main>
+    // detect browser
+    const browser = detectAgent();
+    // display
+    if (browser === "safari") {
+        console.log(browser);
+        return (
+            <div className="px-2 md:p-0 flex flex-col align-middle justify-center h-screen bg-gradient-to-t from-slate-900 to-cyan-900">
+                <title>{pageTitle}</title>
+                {/* TODO: add nav for desktop */}
+                {/* card container */}
+                <div className="self-center md:rounded-lg h-5/6 w-screen sm:w-4/5 overflow-auto">
+                    <main>
+                        {children}
+                    </main>
             </div>
             {/* mobile navigation */}
-            <div className="flex justify-evenly h-2 p-3 mt-4 h-1 mb-44 md:mb-0">
-                <Link to="/"><HomeIcon className="h-10 w-10 text-white hover:text-indigo-300 focus:text-indigo-300"/></Link>
-                <Link to="/more-info"><InformationCircleIcon className="h-10 w-10 text-white hover:text-indigo-300"/></Link>
-            </div>
+                <div className="flex justify-evenly h-2 p-3 mt-4 h-1 mb-44 md:mb-0">
+                    <Link to="/"><HomeIcon className="h-10 w-10 text-white hover:text-indigo-300 focus:text-indigo-300"/></Link>
+                    <Link to="/more-info"><InformationCircleIcon className="h-10 w-10 text-white hover:text-indigo-300"/></Link>
+                </div>
         </div>
-    )
+        )
+    } else {
+        console.log("everything else");
+        return (
+            <div className="px-2 md:p-0 flex flex-col align-middle justify-center h-screen bg-gradient-to-t from-slate-900 to-cyan-900">
+                <title>{pageTitle}</title>
+                {/* TODO: add nav for desktop */}
+                {/* card container */}
+                <div className="self-center md:rounded-lg h-5/6 w-screen sm:w-4/5 overflow-auto">
+                    <main>
+                        {children}
+                    </main>
+                </div>
+                {/* mobile navigation */}
+                <div className="flex justify-evenly h-2 p-3 mt-4 h-1 md:mb-0">
+                    <Link to="/"><HomeIcon className="h-10 w-10 text-white hover:text-indigo-300 focus:text-indigo-300"/></Link>
+                    <Link to="/more-info"><InformationCircleIcon className="h-10 w-10 text-white hover:text-indigo-300"/></Link>
+                </div>
+            </div>
+        )
+    }
 }
 
 // export layout for use
