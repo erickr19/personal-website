9c72cf4488e55422bb2c795ac90b750004fd1222
Erick Reyes <erickreyes1902@gmail.com>
2022-07-30 22:30:33 -0500

    Moved navigation to the top of the screen.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,21 +7,22 @@ import { Link } from "gatsby";
 
 const Layout = ({ pageTitle, children }) => {
     // TODO: Add dynamic page titles
-    
+
     return (
         <div className="px-2 md:p-0 flex flex-col align-middle justify-center h-screen bg-gradient-to-t from-slate-900 to-cyan-900">
             <title>{pageTitle}</title>
             {/* TODO: add nav for desktop */}
+            
+            {/* mobile navigation */}
+            <div className="flex justify-evenly h-2 p-3 h-1 mb-10 md:mb-0">
+                <Link to="/"><HomeIcon className="h-10 w-10 text-white hover:text-indigo-300 focus:text-indigo-300" /></Link>
+                <Link to="/more-info"><InformationCircleIcon className="h-10 w-10 text-white hover:text-indigo-300" /></Link>
+            </div>
             {/* card container */}
             <div className="self-center md:rounded-lg h-5/6 w-screen sm:w-4/5 overflow-auto">
                 <main>
                     {children}
                 </main>
-            </div>
-            {/* mobile navigation */}
-            <div className="flex justify-evenly h-2 p-3 mt-4 h-1 mb-44 md:mb-0">
-                <Link to="/"><HomeIcon className="h-10 w-10 text-white hover:text-indigo-300 focus:text-indigo-300"/></Link>
-                <Link to="/more-info"><InformationCircleIcon className="h-10 w-10 text-white hover:text-indigo-300"/></Link>
             </div>
         </div>
     )
