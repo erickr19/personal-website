35772692a9c923ee61b61e50b5dba1bc7e7d5a6f
Erick Reyes <erickreyes1902@gmail.com>
2022-07-30 19:23:22 -0500

    Fixed function to add user agent detection.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,15 @@
 import * as React from 'react';
 import { HomeIcon, InformationCircleIcon } from '@heroicons/react/outline';
 import { Link } from "gatsby";
-import detectAgent from '../functions/detect-agent';
+import Bowser from "bowser";
 // define layout
 
 const Layout = ({ pageTitle, children }) => {
     // detect browser
-    const browser = detectAgent();
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    console.log(browser.parsedResult.browser.name);
     // display
-    if (browser === "safari") {
+    if (browser.parsedResult.browser.name === "Safari") {
         console.log(browser);
         return (
             <div className="px-2 md:p-0 flex flex-col align-middle justify-center h-screen bg-gradient-to-t from-slate-900 to-cyan-900">
